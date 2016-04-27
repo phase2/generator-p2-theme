a948fdd0a720c39671cbc633c85ecfe8e3bba8c9
Evan Lovely <evan@evanlovely.com>
2016-04-26 23:34:28 -0700

    adding extra script file

--- generators/js/templates/js/other-script.js
+++ generators/js/templates/js/other-script.js
@@ -3,9 +3,9 @@
   'use strict';
 
   // A demo Drupal behavior
-  Drupal.behaviors.customBehavior = {
+  Drupal.behaviors.otherCustomBehavior = {
     attach: function (context, settings) {
-      // $('body', context).prepend('Demo drupal behavior in js/script.js');
+      // $('body', context).prepend('Demo drupal behavior in js/other-script.js');
       // console.log('Drupal behavior from js/script.js');
     }
   };
