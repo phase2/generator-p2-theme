0a7d28d6272969e5c60166360b600e6e00939f2b
Evan Lovely <evan@evanlovely.com>
2016-04-26 22:20:08 -0700

    adding our prompts and templating

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,47 +2,70 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var myPrompts = require('./prompts.js');
+var _ = require('lodash');
+var options = {};
 
 module.exports = yeoman.Base.extend({
+  initializing: function () {
+    this.pkg = require('../../package.json');
 
-  prompting: function () {
+    if (!this.options.skipWelcome) {
+      // Have Yeoman greet the user.
+      this.log(yosay(
+        'Welcome to the remarkable ' + chalk.red('P2 Theme') + ' generator! ' + this.pkg.version + '\nPlease be in the folder you want files in now.'
+      ));
+    }
+    options.themePath = '';
+    options = _.assign(options, this.options);
+  },
+
+ prompting: function () {
     var done = this.async();
+    var prompts = [];
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the super-duper ' + chalk.red('p2-theme') + ' generator!'
-    ));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    myPrompts.forEach(function (item) {
+      if (_.isUndefined(options[item.name])) {
+        prompts.push(item);
+      }
+    });
 
     this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someAnswer;
-
+      options = _.assign(options, props);
       done();
-    }.bind(this));
+    });
+
+    // this.composeWith('pattern-lab-starter:extras', {options: options}, {
+    //   local: path.resolve(__dirname, '../extras')
+    // });
+
   },
 
   writing: function () {
     // Copy all non-dotfiles
-    this.fs.copy(
+    this.fs.copyTpl(
       this.templatePath('**/*'),
-      this.destinationRoot()
+      this.destinationRoot(),
+      options
     );
 
     // Copy all dotfiles
-    this.fs.copy(
+    this.fs.copyTpl(
       this.templatePath('.*'),
-      this.destinationRoot()
+      this.destinationRoot(),
+      options
     );
   },
 
   install: function () {
-    // this.installDependencies();
+    if (options.installDeps) {
+      console.log('Running "npm install"...');
+      // this.npmInstall();
+      console.log('Running "composer install"...');
+      // this.spawnCommandSync('composer', ['install'], {
+      //   cwd: path.join(options.themePath, 'pattern-lab')
+      // });
+    }
   }
+
 });
