a763e616f8e3efae6e10c13b0d04ca315d92f62c
Evan Lovely <evan@evanlovely.com>
2016-04-29 16:38:35 -0700

    adding comment

--- generators/pl/index.js
+++ generators/pl/index.js
@@ -4,11 +4,12 @@ var yeoman = require('yeoman-generator');
 module.exports = yeoman.Base.extend({
 
   writing: function () {
+    console.log('Installing pattern-lab/edition-drupal-standard');
     this.spawnCommand('composer', [
       'create-project',
       'pattern-lab/edition-drupal-standard',
-      'pattern-lab'
-    ]);
+      'pattern-lab' // directory to place in
+    ]
   }
 
 });
